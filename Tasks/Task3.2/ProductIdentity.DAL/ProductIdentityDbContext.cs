fdd71cb03722773076d91db84328ba0bd1c636c9
EroNWs <merengrmz@gmail.com>
2024-03-16 16:06:12 +0300

    feat: Integrate IdentityDbContext with custom User model
    Implemented ProductIdentityDbContext as an extension of IdentityDbContext, incorporating the custom User model. This setup integrates the ASP.NET Core Identity system with our application's data model, including a DbSet for Products, to manage user authentication, authorization, and product data within the same context.

--- Tasks/Task3.2/ProductIdentity.DAL/ProductIdentityDbContext.cs
+++ Tasks/Task3.2/ProductIdentity.DAL/ProductIdentityDbContext.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ProductIdentity.Models;
 
 namespace ProductIdentity.DAL;
 
-public class ProductIdentityDbContext : DbContext
+public class ProductIdentityDbContext :IdentityDbContext<User>
 {
     public DbSet<Product> Products { get; set; }
     public ProductIdentityDbContext(DbContextOptions<ProductIdentityDbContext> options)
                 : base(options)
     {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+ 
     }
 
 }
