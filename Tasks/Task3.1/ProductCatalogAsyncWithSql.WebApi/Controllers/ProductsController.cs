3f8f3d1826be7fd6b9af0bcc60bca0808a8dd8b3
EroNWs <merengrmz@gmail.com>
2024-03-16 15:00:20 +0300

    feat: Add ProductsController for CRUD operations
    Added a new ProductsController to handle CRUD operations for products using the ASP.NET Core Web API. This controller utilizes dependency injection to interact with the IProductRepository for performing asynchronous database operations, thereby supporting GET, POST, PUT, and DELETE HTTP methods to manage product entities effectively.

---
+++ Tasks/Task3.1/ProductCatalogAsyncWithSql.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalogAsyncWithSql.Infrastracture.Interface;
+using ProductCatalogAsyncWithSql.Models;
+
+namespace ProductCatalogAsyncWithSql.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return Ok(products);
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            await _productRepository.AddAsync(product);
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            await _productRepository.UpdateAsync(product);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
