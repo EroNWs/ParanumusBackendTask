dde4527ab6dc8f6d837edd6eefee95836684f643
EroNWs <merengrmz@gmail.com>
2024-03-16 17:03:23 +0300

    feat: Setup Authentication and Identity configuration
    Integrated ASP.NET Core Identity into the project for authentication and user management. Configured the Identity system with custom settings for password complexity and unique email requirement. Extended the service collection to include Identity configuration and connected it with the ProductIdentityDbContext.

--- Tasks/Task3.2/ProductIdentity.Infrastracture/Extensions/RepositoryExtensions.cs
+++ Tasks/Task3.2/ProductIdentity.Infrastracture/Extensions/RepositoryExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using ProductIdentity.DAL;
 using ProductIdentity.Infrastracture.Interface;
+using ProductIdentity.Models;
 
 namespace ProductIdentity.Infrastracture.Extensions;
 
@@ -8,6 +11,22 @@ public static class RepositoryExtensions
     public static IServiceCollection AddInfrastractureExtensions(this IServiceCollection services)
     {
         services.AddScoped<IProductRepository, ProductRepository>();
+
+        return services;
+    }
+    public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+    {
+        var builder = services.AddIdentity<User, IdentityRole>(opts =>
+        {
+            opts.Password.RequireDigit = true;
+            opts.Password.RequireLowercase = false;
+            opts.Password.RequireUppercase = false;
+            opts.Password.RequireNonAlphanumeric = false;
+            opts.Password.RequiredLength = 6;
+            opts.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<ProductIdentityDbContext>()
+        .AddDefaultTokenProviders();
 
         return services;
     }
--- Tasks/Task3.2/ProductIdentity.WebApi/Program.cs
+++ Tasks/Task3.2/ProductIdentity.WebApi/Program.cs
@@ -8,11 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ProductIdentityDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDb")));
 
 builder.Services.AddInfrastractureExtensions();
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication();
+builder.Services.ConfigureIdentity();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
