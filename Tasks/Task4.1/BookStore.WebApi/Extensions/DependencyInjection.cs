ab2e3586a535b2bdc45d96f34b5120958fd69b40
EroNWs <merengrmz@gmail.com>
2024-03-17 20:02:10 +0300

    feat: Define message constants for application feedback
    Introduced a Messages class in the BookStore.Business.Constants namespace to centralize application feedback messages. This includes constants for success, failure, and state-specific messages used across various operations like listing, adding, updating, and deleting entities, as well as user management. Streamlines user feedback and error handling.

---
+++ Tasks/Task4.1/BookStore.WebApi/Extensions/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Dal.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.WebApi.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services,
+    IConfiguration configuration) =>
+    services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ParanamusDbContext")));
+    }
+}
--- Tasks/Task4.1/BookStore.WebApi/Program.cs
+++ Tasks/Task4.1/BookStore.WebApi/Program.cs
@@ -1,11 +1,28 @@
 
 
+using BookStore.WebApi.Extensions;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureSqlContext(builder.Configuration);
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 
-builder.Services.AddControllers();
+
+
 builder.Services.AddEFCoreServices(builder.Configuration);
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
